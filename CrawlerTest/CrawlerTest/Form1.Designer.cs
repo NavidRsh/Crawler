bb5ff1e2dedd6652cd446832a86ce75551e43ba4
NavidRsh <navidroshdieh@yahoo.com>
2020-08-05 17:17:43 +0430

    Add files via upload

---
+++ CrawlerTest/CrawlerTest/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CrawlerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnGetTse = new System.Windows.Forms.Button();
+            this.btnGetList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(713, 31);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 33);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(695, 20);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "https://www.sahamyab.com/hashtag/%D9%81%D9%88%D9%84%D8%A7%D8%AF";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 60);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(776, 347);
+            this.txtResult.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // btnGetTse
+            // 
+            this.btnGetTse.Location = new System.Drawing.Point(551, 415);
+            this.btnGetTse.Name = "btnGetTse";
+            this.btnGetTse.Size = new System.Drawing.Size(75, 23);
+            this.btnGetTse.TabIndex = 3;
+            this.btnGetTse.Text = "tse";
+            this.btnGetTse.UseVisualStyleBackColor = true;
+            this.btnGetTse.Click += new System.EventHandler(this.btnGetTse_Click);
+            // 
+            // btnGetList
+            // 
+            this.btnGetList.Location = new System.Drawing.Point(470, 415);
+            this.btnGetList.Name = "btnGetList";
+            this.btnGetList.Size = new System.Drawing.Size(75, 23);
+            this.btnGetList.TabIndex = 4;
+            this.btnGetList.Text = "Get List";
+            this.btnGetList.UseVisualStyleBackColor = true;
+            this.btnGetList.Click += new System.EventHandler(this.btnGetList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetList);
+            this.Controls.Add(this.btnGetTse);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnGetTse;
+        private System.Windows.Forms.Button btnGetList;
+    }
+}
+
---
+++ CrawlerTest/CrawlerTest/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrawlerTest
+{
+    public partial class Form1 : Form
+    {
+        private string GeneratedSource { get; set; }
+        private string URL { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Rstring; 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //WebRequest myWebRequest;
+            //WebResponse myWebResponse;
+            //String URL = txtUrl.Text;
+
+            //myWebRequest = WebRequest.Create(URL);
+            //myWebResponse = myWebRequest.GetResponse();//Returns a response from an Internet resource
+
+            //Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet
+            //                                                          //and save it in the stream
+
+            //StreamReader sreader = new StreamReader(streamResponse);//reads the data stream
+            //Rstring = sreader.ReadToEnd();//reads it to the end
+            //var Links = GetNewLinks(Rstring);//gets the links only
+
+            ////textBox2.Text = Rstring;
+            ////textBox3.Text = Links;
+            //streamResponse.Close();
+            //sreader.Close();
+            //myWebResponse.Close();
+
+            ////////////////////////////////////////////////////
+
+            GetGeneratedHTML(txtUrl.Text); 
+        }
+
+        public string GetGeneratedHTML(string url)
+        {
+            string URL = url;
+
+            Thread t = new Thread(new ThreadStart(WebBrowserThread));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+
+            return GeneratedSource;
+        }
+
+        private void WebBrowserThread()
+        {
+            WebBrowser wb = new WebBrowser();
+            wb.Navigate(URL);
+
+            wb.DocumentCompleted +=
+                new WebBrowserDocumentCompletedEventHandler(
+                    wb_DocumentCompleted);
+
+            while (wb.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+
+            //Added this line, because the final HTML takes a while to show up
+            GeneratedSource = wb.Document.Body.InnerHtml;
+
+            wb.Dispose();
+        }
+
+        private void wb_DocumentCompleted(object sender,
+            WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser wb = (WebBrowser)sender;
+            GeneratedSource = wb.Document.Body.InnerHtml;
+        }
+
+
+
+        public ISet<string> GetNewLinks(string content)
+        {
+            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+            ISet<string> newLinks = new HashSet<string>();
+            foreach (var match in regexLink.Matches(content))
+            {
+                if (!newLinks.Contains(match.ToString()))
+                    newLinks.Add(match.ToString());
+            }
+
+            return newLinks;
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnGetTse_Click(object sender, EventArgs e)
+        {
+            string html = string.Empty;
+            string url = @"http://tsetmc.com/tsev2/data/instinfofast.aspx?i=7745894403636165&c=23+";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+        }
+
+        private void btnGetList_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.tsetmc.com/loader.aspx?ParTree=111C1115";
+        }
+    }
+}
